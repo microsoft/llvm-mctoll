a0a62f7334ee62994cf8ae27255bf375c072c27e
Aaron Smith <aaron.smith@microsoft.com>
2019-04-12 13:50:11 -0700

    Fix formatting; NFC

--- X86/X86RegisterUtils.h
+++ X86/X86RegisterUtils.h
@@ -1,4 +1,4 @@
-//==-- X86RegisterUtils.h - Binary raiser utility llvm-mctoll -----*- C++ -*-===//
+//==-- X86RegisterUtils.h - Binary raiser utility llvm-mctoll ---*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.

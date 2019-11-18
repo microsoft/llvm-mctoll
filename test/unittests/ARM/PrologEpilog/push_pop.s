6f855b2c1b130e242831af0e2cab60f8220a9c87
Bharadwaj Yadavalli <Bharadwaj.Yadavalli@microsoft.com>
2019-11-18 01:26:15 +0000

    [Tests] Contributes changes that re-enable temporarily disabled ARM tests along with fixes.

--- test/unittests/ARM/PrologEpilog/push_pop.s
+++ test/unittests/ARM/PrologEpilog/push_pop.s
@@ -1,6 +1,5 @@
-# UNSUPPORTED: -linux-
 # RUN: clang -target arm -mfloat-abi=soft -c -o %t.o %s
-# RUN: llvm-mctoll -d -print-after-all %t.o 2>&1 | FileCheck %s
+# RUN: llvm-mctoll -d -print-after-all -debug %t.o 2>&1 | FileCheck %s
 
 # CHECK: ARMEliminatePrologEpilog start
 # CHECK: Frame Objects:
